02deb254e4c162ece7d0b917696ba2e73c729d2e
Philippe Miranda <pm.capitani@itmss.com.br>
2019-05-29 16:49:04 -0300

    Removed package-locked

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <p> A simple chart made with chart.js and Vue, for learning process
          <br> In this example i consume data from this
          <a href="https://www.quandl.com/data/CEPEA-Center-for-Applied-Studies-on-Applied-Economics-Brazil">Quandl dataset</a>  
-         database and explore some of the core concepts of Vuejs and Chart.js .
+         and explore some of the core concepts of Vuejs and Chart.js .
 
       </p>
     </div>
